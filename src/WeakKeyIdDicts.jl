81900752ef0f5d0f756034c725042461ce8bc474
David Little <david.frank.little@gmail.com>
2024-04-03 10:40:14 -0400

    cleanup up doc generation

--- src/WeakKeyIdDicts.jl
+++ src/WeakKeyIdDicts.jl
@@ -33,15 +33,16 @@ Base.hash(wr::WeakRefForWeakDict, h::UInt) = Base.hash_uint(3h - objectid(wr.w.v
 """
     WeakKeyIdDict([itr])
 
-`WeakKeyIdDict()` constructs a hash table where the keys are weak
-references to objects which may be garbage collected even when
-referenced in a hash table.
+`WeakKeyIdDict()` constructs a hash table where the keys are weak references to objects
+which may be garbage collected even when referenced in a hash table.
 
-See [`Dict`](@ref) for further help.  Note, unlike [`Dict`](@ref),
-`WeakKeyIdDict` does not convert keys on insertion, as this would imply the key
-object was unreferenced anywhere before insertion.
+See [`Dict`](https://docs.julialang.org/en/v1/base/collections/#Base.Dict) for further help.
+Note, unlike [`Dict`](https://docs.julialang.org/en/v1/base/collections/#Base.Dict),
+`WeakKeyIdDict` does not convert keys on insertion, as this would imply the key object was
+unreferenced anywhere before insertion.
 
-See also [`WeakRef`](@ref), [`WeakKeyDict`](@ref).
+See also [`WeakRef`](https://docs.julialang.org/en/v1/base/base/#Core.WeakRef),
+[`WeakKeyDict`](https://docs.julialang.org/en/v1/base/collections/#Base.WeakKeyDict).
 """
 mutable struct WeakKeyIdDict{K,V} <: AbstractDict{K,V}
     ht::Dict{WeakRefForWeakDict,V}
